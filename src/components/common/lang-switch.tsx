3632002709c576d5bd014053578e632295d97ae9
Moneer <xv.neer.business@gmail.com>
2025-04-29 11:45:53 +0300

    handle language switch

---
+++ src/components/common/lang-switch.tsx
@@ -0,0 +1,62 @@
+// components/LanguageSwitcher.tsx
+
+"use client"
+
+import { LOCALES } from "@/config"
+import { usePathname } from "@/lib/i18n/navigation"
+import { Button, Menu } from "@mantine/core"
+import { useLocale } from "next-intl"
+import { useRouter } from "next/navigation"
+import { useCallback } from "react"
+import { RiyalIcon } from "../icons"
+import { Globe } from "lucide-react"
+
+const LanguageSwitcher = () => {
+  const pathname = usePathname()
+  const router = useRouter()
+  const currentLocale = useLocale()
+
+  const changeLanguage = useCallback(
+    (newLocale: string) => {
+      const searchParams = window.location.search
+      const newPath = `/${newLocale}${pathname}${searchParams}`
+      router.push(newPath)
+    },
+    [router, pathname]
+  )
+
+  return (
+    <>
+      <Menu shadow="md" width={200}>
+        <Menu.Target>
+          <Button
+            size="sm"
+            className="max-md:px-0.5"
+            variant="subtle"
+            color="black"
+            leftSection={<Globe strokeWidth={1.3} />}
+          >
+            {currentLocale}
+          </Button>
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          {LOCALES.map((element) => {
+            return (
+              <Menu.Item
+                onClick={() => changeLanguage(element)}
+                disabled={element === currentLocale}
+                key={element}
+                leftSection={<RiyalIcon />}
+              >
+                {element}
+              </Menu.Item>
+            )
+          })}
+        </Menu.Dropdown>
+      </Menu>
+    </>
+  )
+}
+
+export default LanguageSwitcher
