f37fcdd89446da913da4e3cf8ad50ff92df01a8f
Moneer <xv.neer.business@gmail.com>
2025-04-29 12:04:26 +0300

    fix hero section for mobile

--- src/app/[locale]/components/hero.tsx
+++ src/app/[locale]/components/hero.tsx
@@ -9,26 +9,33 @@ export default async function Hero() {
   const t = await getTranslations("home.hero")
   const [unitTypes, cities] = await Promise.all([getUnitTypes(), getCities()])
   return (
-    <section className=" h-[70svh] lg:h-[calc(100svh_-_74px)] flex items-center ">
-      <Image
-        src={heroBackground}
-        alt="background"
-        className="w-full h-full object-cover  absolute inset-0"
-      />
-      <div className="container relative text-white mx-auto">
-        <Stack gap={"xl"} mt={"90"}>
-          <Stack gap={"xs"}>
-            <Title order={1}>{t("title")}</Title>
-            <Text className="lg:max-w-3xl" fz={28}>
-              {t("description")}
-            </Text>
+    <>
+      <section className=" h-[30svh] lg:h-[calc(100svh_-_74px)] flex items-center relative ">
+        <Image
+          src={heroBackground}
+          alt="background"
+          className="w-full h-full object-cover  absolute inset-0"
+        />
+        <div className="container relative text-white mx-auto">
+          <Stack gap={"xl"} mt={{ md: "90" }}>
+            <Stack gap={"xs"}>
+              <Title
+                className="text-3xl max-md:text-center md:text-[52px]"
+                order={1}
+              >
+                {t("title")}
+              </Title>
+              <Text className="max-md:text-center  md:text-3xl lg:max-w-3xl">
+                {t("description")}
+              </Text>
+            </Stack>
+            <Space visibleFrom="md" />
+            <div className=" hidden md:block max-w-5xl">
+              <SearchBar cities={cities} unitTypes={unitTypes} />
+            </div>
           </Stack>
-          <Space />
-          <div className=" max-w-5xl">
-            <SearchBar cities={cities} unitTypes={unitTypes} />
-          </div>
-        </Stack>
-      </div>
-    </section>
+        </div>
+      </section>
+    </>
   )
 }
