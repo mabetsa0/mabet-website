e3924b8afcb8c3570d247e2869273f3245380cf7
Moneer <xv.neer.business@gmail.com>
2025-05-03 14:37:33 +0300

    fix build error

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,4 +1,4 @@
-import logo from "./logo.svg"
+import logo from "./main-logo.svg"
 import calenderIn from "./calendar-check-in-01.svg"
 import calenderOut from "./calendar-check-out-01.svg"
 import heroBackground from "./Hero-Image.png"
