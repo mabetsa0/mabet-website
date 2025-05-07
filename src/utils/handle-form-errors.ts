516d2a78dddad57071b2215ee4298346b39689cf
Moneer <xv.neer.business@gmail.com>
2025-05-07 17:07:03 +0300

    create a util to handle form errors

---
+++ src/utils/handle-form-errors.ts
@@ -0,0 +1,31 @@
+import { UseFormReturnType } from "@mantine/form"
+import axios from "axios"
+export type ErrorResponse<T> = {
+  message: string
+  errors?: {
+    [key in keyof T]?: string[]
+  }
+}
+
+export const handleFormError = <T>(
+  error: unknown,
+  form: UseFormReturnType<T>
+) => {
+  if (axios.isAxiosError(error) && error.response?.status) {
+    const responseError = error.response.data as ErrorResponse<{ "": "" }>
+
+    form.setFieldError("root", responseError.message)
+
+    if (responseError.errors) {
+      for (const key in responseError.errors) {
+        form.setFieldError(
+          key,
+          responseError.errors![key as keyof typeof responseError.errors]![0]
+        )
+      }
+    }
+
+    return
+  }
+  form.setFieldError("root", "server error")
+}
