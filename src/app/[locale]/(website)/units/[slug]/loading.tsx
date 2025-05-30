2a20adb7dd1222d1bdd8f1d53418eca0cc72643d
Moneer <xv.neer.business@gmail.com>
2025-05-30 13:11:45 +0300

    working on scorll issue

--- src/app/[locale]/(website)/units/[slug]/loading.tsx
+++ src/app/[locale]/(website)/units/[slug]/loading.tsx
@@ -1,14 +1,16 @@
 "use client"
-import { Loader } from "@mantine/core"
+import { Loader2 } from "lucide-react"
 import { useEffect } from "react"
 
 const Loading = () => {
   useEffect(() => {
     window.scrollTo(0, 0)
+    console.log("🚀 ~ useEffect ~ window.scrollTo:", "scroll to top")
   }, [])
+
   return (
     <div className="flex items-center justify-center h-[100vh] md:h-[10vh]">
-      <Loader />
+      <Loader2 className="w-2.5 h-2.5 text-primary animate-spin" />
     </div>
   )
 }
