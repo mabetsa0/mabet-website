11e380195a84e37690e34493ee69dc03674e848a
Moneer <xv.neer.business@gmail.com>
2025-05-31 12:41:29 +0300

    fix pagination

--- src/app/[locale]/(website)/units/components/pagination.tsx
+++ src/app/[locale]/(website)/units/components/pagination.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Pagination as MantinePagination } from "@mantine/core"
 import { parseAsInteger, useQueryState } from "nuqs"
+import { useEffect } from "react"
 
 type Props = {
   total?: number
@@ -8,8 +9,10 @@ type Props = {
 
 const Pagination = (props: Props) => {
   const [page, setPage] = useQueryState("page", parseAsInteger.withDefault(1))
+  useEffect(() => {
+    if (props.total && Number(page) > Number(props.total)) setPage(props.total)
+  }, [page, props.total, setPage])
   if (!props.total || props.total <= 1) return null
-
   return (
     <div className=" flex justify-center py-4">
       <MantinePagination value={page} onChange={setPage} total={props.total} />
