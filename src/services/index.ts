31cda1a89b087f2248e80b3f66f37449beec271b
Moneer <xv.neer.business@gmail.com>
2025-06-04 10:47:17 +0300

    handle production

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,11 +4,11 @@ import axios from "axios"
 import { getLocale } from "next-intl/server"
 import { getSession } from "@/utils/get-session"
 
-// const baseURL =
-//   process.env.NEXT_PUBLIC_TEST === "test"
-//     ? "https://mabet.dev/api/v2"
-//     : "https://app.mabet.com.sa/api/v2"
-const baseURL = "https://mabet.dev/api/v2"
+const baseURL =
+  process.env.NEXT_PUBLIC_TEST === "test"
+    ? "https://mabet.dev/api/v2"
+    : "https://app.mabet.com.sa/api/v2"
+// const baseURL = "https://mabet.dev/api/v2"
 
 const Mabet = axios.create({
   baseURL: baseURL,
