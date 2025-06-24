07805c07a8e73cc84be35ed90c53ff3840fdfc0e
Moneer <xv.neer.business@gmail.com>
2025-06-24 12:41:31 +0300

    create contact us

---
+++ src/app/[locale]/(website)/private/contact-us/components/contact-form.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import Mabeet from "@/api"
+
+import { handleFormError } from "@/utils/handle-form-errors"
+import {
+  Button,
+  Group,
+  Space,
+  Stack,
+  Text,
+  Textarea,
+  TextInput,
+} from "@mantine/core"
+import { isEmail, useForm } from "@mantine/form"
+import { File, Mail, Phone, UserIcon } from "lucide-react"
+import { useTranslations } from "next-intl"
+const ContactForm = () => {
+  const t = useTranslations()
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      message: "",
+      subject: "",
+    },
+  })
+  const onSubmit = form.onSubmit(async (data) => {
+    try {
+      const response = await Mabeet.post("/contact-us", data)
+    } catch (error) {
+      handleFormError(error, form)
+    }
+  })
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Stack>
+        <Stack maw={550}>
+          <Text className="text-h5  font-medium">
+            {t("user.contact-form.title")}
+          </Text>
+          <div className="border border-[#F3F3F3] rounded-lg">
+            <TextInput
+              required
+              withAsterisk={false}
+              placeholder={t("user.contact-form.subject.placeholder")}
+              variant="unstyled"
+              classNames={{
+                input: "px-1 mt-[4px]",
+                label: " border-b w-full border-b-[#F3F3F3] px-1 py-[12px]",
+                error: "px-1   mb-0.5 ",
+              }}
+              label={
+                <Group gap={"xs"} component={"span"}>
+                  <File strokeWidth={1.25} className="text-primary" />
+                  {t("user.contact-form.subject.label")}
+                </Group>
+              }
+            />
+          </div>
+          <div className="border border-[#F3F3F3] rounded-lg">
+            <Textarea
+              rows={3}
+              required
+              withAsterisk={false}
+              placeholder={t("user.contact-form.message.label")}
+              variant="unstyled"
+              classNames={{
+                input: "px-1 mt-[4px]",
+                label: " border-b w-full border-b-[#F3F3F3] px-1 py-[12px]",
+                error: "px-1   mb-0.5 ",
+              }}
+              label={
+                <Group gap={"xs"} component={"span"}>
+                  <Mail strokeWidth={1.25} className="text-primary" />
+                  {t("user.contact-form.message.placeholder")}
+                </Group>
+              }
+            />
+          </div>
+
+          <Text className="md:text-lg" c={"#767676"}>
+            {t("user.contact-form.description")}
+          </Text>
+          <Space />
+          <Button type="submit" loading={form.submitting}>
+            {t("user.contact-form.submit")}
+          </Button>
+          {form.errors.root && (
+            <Text ta={"center"} size="sm" c={"red"}>
+              {form.errors.root}
+            </Text>
+          )}
+        </Stack>
+      </Stack>
+    </form>
+  )
+}
+
+export default ContactForm
---
+++ src/app/[locale]/(website)/private/contact-us/page.tsx
@@ -0,0 +1,91 @@
+import { Card, SimpleGrid, Space, Stack, Text } from "@mantine/core"
+import { Clock, Gift, Mail, Phone, Smartphone } from "lucide-react"
+import { useTranslations } from "next-intl"
+import ContactForm from "./components/contact-form"
+
+const Page = () => {
+  const t = useTranslations()
+
+  const contactInfo = [
+    {
+      icon: Smartphone,
+      content: "+966-567570014",
+      type: "phone",
+    },
+    {
+      icon: Mail,
+      content: "help@mabet.com.sa",
+      type: "mail",
+    },
+    {
+      icon: Clock,
+      content: "من 09:30 صباحاً إلى 01:00 ليلاً",
+    },
+  ]
+
+  return (
+    <Stack>
+      <Stack>
+        <Text className="text-h3 md:text-h2 font-bold">
+          {t("user.contact.title")}
+        </Text>
+        <Text className="md:text-lg">{t("user.contact.description")}</Text>
+      </Stack>
+
+      <Space />
+
+      <Stack>
+        <Text className="text-h4 font-bold">
+          {t("user.contact.customer-services")}
+        </Text>
+        <SimpleGrid cols={{ base: 1, sm: 3 }}>
+          {contactInfo.map((ele, index) => {
+            return (
+              <Card
+                p={"xs"}
+                key={index}
+                withBorder
+                className="border-[#F3F3F3]"
+              >
+                <div className="flex mb-0.5 rounded-lg text-primary bg-primary/15 items-center justify-center shrink-0 w-3.5  aspect-square">
+                  <ele.icon size={28} strokeWidth={1.2} />
+                </div>
+
+                {ele.type === "phone" ? (
+                  <Text
+                    component="a"
+                    href={`tel:${ele.content}`}
+                    dir="ltr"
+                    className="text-h5 font-medium rtl:text-right "
+                  >
+                    {ele.content}
+                  </Text>
+                ) : ele.type === "mail" ? (
+                  <Text
+                    component="a"
+                    href={`mail:${ele.content}`}
+                    dir="ltr"
+                    className="text-h5 font-medium rtl:text-right "
+                  >
+                    {ele.content}
+                  </Text>
+                ) : (
+                  <Text
+                    dir="ltr"
+                    className="text-h5 font-medium rtl:text-right "
+                  >
+                    {ele.content}
+                  </Text>
+                )}
+              </Card>
+            )
+          })}
+        </SimpleGrid>
+      </Stack>
+      <Space />
+      <ContactForm />
+    </Stack>
+  )
+}
+
+export default Page
