88d5ffd6c2cd2b3e47c43a53651b3757cff09816
Moneer <xv.neer.business@gmail.com>
2025-06-26 11:19:16 +0300

    create privae pages

--- src/app/[locale]/(website)/private/bookings/[booking_id]/page.tsx
+++ src/app/[locale]/(website)/private/bookings/[booking_id]/page.tsx
@@ -1,178 +1,237 @@
-import dynamic from "next/dynamic"
 import { notFound } from "next/navigation"
 
-import axios from "axios"
-import { ArrowLeft, MapPin, Tag } from "lucide-react"
-import { GetBooking } from "./helpers/get-booking"
-import { GetUnit } from "../../../units/[slug]/get-unit"
 import { RiyalIcon } from "@/components/icons"
-import { Button, Divider } from "@mantine/core"
-import ImageGallery from "../../../units/[slug]/components/image-gallery"
-import ImageSlider from "../../../units/[slug]/components/image-slider"
-import { UnitContextProvider } from "../../../units/[slug]/context/unit-context"
+import BackButton from "@/components/ui/back-button"
+import { Link } from "@/lib/i18n/navigation"
+import {
+  Box,
+  Button,
+  Divider,
+  Group,
+  SimpleGrid,
+  Space,
+  Spoiler,
+  Stack,
+  Text,
+} from "@mantine/core"
+import axios from "axios"
+import dayjs from "dayjs"
+import { MapPin, Tag } from "lucide-react"
+import { getTranslations } from "next-intl/server"
+import { GetBooking } from "./helpers/get-booking"
 
 type Props = {
   params: Promise<{
     booking_id: string
+    locale: string
   }>
 }
 
 const page = async (props: Props) => {
   try {
     const params = await props.params
+    dayjs.locale(params.locale)
     const booking = await GetBooking(params.booking_id)
-    const unit = await GetUnit({ slug: booking.unit.id + "" })
+    const unit = booking.unit
+    const t = await getTranslations("booking-page")
     return (
-      <UnitContextProvider value={unit}>
-        <ImageGallery />
-        <ImageSlider />
-        <section className="relative z-10 bg-white max-lg:-mt-1 max-lg:rounded-large">
-          <div className="container">
-            <div className="flex gap-1 max-lg:flex-col">
-              <div className="py-2 lg:w-2/3">
-                <div className="space-y-1.5">
-                  <h1 className="text-2xl">
-                    تفاصيل الحجز رقم #{params.booking_id}
-                  </h1>
-                  <div className="flex gap-1">
-                    <div className="w-1/2 rounded bg-green-300/15 p-[12px]">
-                      <p className="text-sm font-medium text-default-600">
-                        وقت الوصول
-                      </p>
-                      <span>{unit.checkin}</span>
-                    </div>
-                    <div className="w-1/2 rounded bg-orange-300/15 p-[12px]">
-                      <p className="text-sm font-medium text-default-600">
-                        وقت المغادرة
-                      </p>
-                      <span>{unit.checkout}</span>
-                    </div>
-                  </div>
-                  <Divider />
+      <Stack>
+        <Space />
+        <Stack>
+          <Text className="text-h3 md:text-h2 font-bold">
+            <BackButton
+              variant="filled"
+              color="primary"
+              size={"lg"}
+              className="me-1"
+            />
+            {t("title", { value: params.booking_id })}
+          </Text>
+          <Text className="md:text-lg">{t("description")}</Text>
+        </Stack>
+        <Stack gap={"lg"}>
+          <Space />
 
-                  <div className="space-y-0.5">
-                    <p className="text-2xl">شروط الحجز</p>
-                    <ul className="list-inside list-disc ps-1">
-                      {booking.unit.terms.map((term, index) => (
-                        <li key={index}>{term}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="space-y-0.5">
-                    <p className="text-2xl">سياسة الإلغاء والاسترجاع</p>
-                    <ul className="list-inside list-disc ps-1">
-                      <li>{booking.cancellation_text}</li>
-                    </ul>
-                  </div>
-                  <Divider />
-                  <div className="space-y-0.5">
-                    <p className="text-2xl">عنوان العقار</p>
-                    <div className="flex items-center gap-2 text-sm">
-                      <MapPin className="size-1" />
-                      <p>{booking.unit.location}</p>
-                    </div>
-                    <Button
-                      fullWidth
-                      className="max-w-sm"
-                      component={"a"}
-                      href={booking.maps_link}
-                      target="_blank"
-                    >
-                      موقع العقار على الخريطة
-                    </Button>
-                  </div>
-                  <Divider />
-                  <div className="space-y-0.5">
-                    <p className="text-2xl">تعلميات الوصول</p>
-                    <ul className="list-inside list-disc ps-1">
-                      {booking.arrival_instructions.map((term, index) => (
-                        <li key={index}>
-                          {term.label}{" "}
-                          {term.content_type === "phone" && (
-                            <a href={`tel:+966${term.content}`}>
-                              {term.content}
-                            </a>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="space-y-0.5">
-                    <p className="text-2xl">بيانات المضيف</p>
-                    <p className="ms-1">اسم المضيف : {booking.partner.name}</p>
-                    <p className="ms-1">
-                      رقم المضيف:{" "}
-                      <a href={`tel:+966${booking.partner.phone}`}>
-                        {booking.partner.phone}
-                      </a>
-                    </p>
-                  </div>
-                  <Divider />
-                  <div className="space-y-[12px]">
-                    <p className="text-2xl">ملخص الحجز</p>
-
-                    <div className="flex items-center justify-between gap-1">
-                      <p>صافي إجمالي السعر في الليلة</p>
-                      <span className="font-bold">
-                        {booking.night_price} <RiyalIcon />
-                      </span>
-                    </div>
-
-                    <div className="flex items-center justify-between gap-1 text-sm text-foreground-500">
-                      <p>عدد الليالي</p>
-                      <span>{booking.duration}</span>
-                    </div>
-                    <div className="flex items-center justify-between gap-1 text-sm text-foreground-500">
-                      <p>رسوم الخدمة </p>
-                      <span>{booking.customer_fees_text}</span>
-                    </div>
-                    <Divider />
-                    <div className="flex items-center justify-between py-1 text-lg font-bold">
-                      <p>إجمالي السعر</p>
-                      <span>{booking.full_payment_text}</span>
-                    </div>
-                    <div>
-                      <Button
-                        component={"a"}
-                        download
-                        href={booking.pdf_link}
-                        leftSection={<Tag strokeWidth={1.2} />}
-                        variant="light"
-                      >
-                        عرض فاتورة الحجز
-                      </Button>
-                    </div>
-                  </div>
-                  <Divider />
-                  <div className="space-y-0.5">
-                    <p className="text-2xl">الدعم وحماية حقوقك</p>
-                    <p className="ms-1">{booking.support.label}</p>
-                    <ul className="list-inside list-disc ps-1">
-                      {booking.support.links.map((link, index) => (
-                        <li key={index} className="flex items-center underline">
-                          <a
-                            target="_blank"
-                            href={link.link}
-                            className="flex items-center gap-2"
-                          >
-                            <img
-                              className="size-1"
-                              src={link.icon_svg}
-                              alt=""
-                            />
-                            {link.label}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
+          <Group align="stretch">
+            <Box className="h-[140px] w-[160px] rounded-md overflow-hidden">
+              <img
+                className="w-full h-full object-cover"
+                src={unit.images[0].image_path}
+                alt={unit.images[0].alt}
+              />
+            </Box>
+            <Stack>
+              <h3 className="text-h4 md:text-h5 font-bold line-clamp-1">
+                {unit.name}{" "}
+                <span className="text-base font-light text-gray-500">
+                  ({unit.code})
+                </span>
+              </h3>
+              <div className="mt-auto">
+                <Link
+                  className=" w-full block mt-auto"
+                  href={`/unit/${unit.id}`}
+                >
+                  <Button fullWidth variant="light" size="sm">
+                    {t("view-unit")}
+                  </Button>
+                </Link>
               </div>
-              <div className="lg:w-1/3"></div>
+            </Stack>
+          </Group>
+
+          <Space />
+
+          <SimpleGrid cols={2}>
+            <div className=" rounded bg-green-300/15 p-[12px]">
+              <p className="text-sm font-medium text-default-600">
+                {t("checkin")}
+              </p>
+              <p className="font-medium ">{booking.checkin_text}</p>
+              <p>{booking.checkin_time}</p>
+            </div>
+            <div className=" rounded bg-red-300/15 p-[12px]">
+              <p className="text-sm font-medium text-default-600">
+                {t("checkout")}
+              </p>
+              <p className="font-medium ">{booking.checkout_text}</p>
+              <p>{booking.checkout_time}</p>
+            </div>
+          </SimpleGrid>
+          <Divider />
+          <div className="space-y-0.5">
+            <p className="text-2xl">{t("unit-location")}</p>
+            <div className="flex items-center gap-2 text-sm">
+              <MapPin className="size-1" />
+              <p>{booking.unit.location}</p>
+            </div>
+            <Button
+              fullWidth
+              className="max-w-sm"
+              component={"a"}
+              href={booking.maps_link}
+              target="_blank"
+            >
+              {t("view-map")}
+            </Button>
+          </div>
+
+          <Divider />
+
+          <div className="space-y-0.5">
+            <p className="text-2xl">{t("arriavl-instructions")}</p>
+            {/* <Spoiler
+              maxHeight={130}
+              showLabel={t("show-more")}
+              hideLabel={t("show-less")}
+              classNames={{
+                content:
+                  "relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 backdrop-blur-sm  before:bg-gradient before:bg-gradient-to-t before:from-white before:to-white/0 ",
+                control:
+                  "bg-primary px-2.5  font-medium py-0.5 rounded-sm text-sm text-white h-2.5 block",
+              }}
+            > */}
+            <ul className="list-inside list-disc ps-1">
+              {booking.arrival_instructions.map((term, index) => (
+                <li key={index}>
+                  {term.label}{" "}
+                  {term.content_type === "phone" && (
+                    <a href={`tel:+966${term.content}`}>{term.content}</a>
+                  )}
+                </li>
+              ))}
+            </ul>
+            {/* </Spoiler> */}
+          </div>
+          <div className="space-y-0.5 bg-gray-50 p-0.5 rounded">
+            <p className="text-2xl">{t("owner-information")}</p>
+            <p className="ms-1">
+              {t("owner-name")}: {booking.partner.name}
+            </p>
+            <p className="ms-1">
+              {t("owner-number")} :{" "}
+              <a
+                className="text-primary"
+                href={`tel:+966${booking.partner.phone}`}
+              >
+                {booking.partner.phone}
+              </a>
+            </p>
+          </div>
+          <Divider />
+
+          <div className="space-y-[12px]">
+            <p className="text-2xl">{t("booking-details")}</p>
+
+            <div className="flex items-center justify-between gap-1">
+              <p>{t("night-price")}</p>
+              <span className="font-bold">
+                {booking.night_price} <RiyalIcon />
+              </span>
+            </div>
+
+            <div className="flex items-center justify-between gap-1 text-sm text-foreground-500">
+              <p>{t("nights-count")}</p>
+              <span>{booking.duration}</span>
+            </div>
+            <div className="flex items-center justify-between gap-1 text-sm text-foreground-500">
+              <p>{t("customer-fees")}</p>
+              <span>{booking.customer_fees_text}</span>
+            </div>
+            <Divider />
+            <div className="flex items-center justify-between py-1 text-lg font-bold">
+              <p>{t("total")}</p>
+              <span>{booking.full_payment_text}</span>
+            </div>
+            <div>
+              <Button
+                component={"a"}
+                download
+                href={booking.pdf_link}
+                leftSection={<Tag strokeWidth={1.2} />}
+                variant="light"
+              >
+                {t("invoice")}
+              </Button>
             </div>
           </div>
-        </section>
-      </UnitContextProvider>
+          <Divider />
+          <div className="space-y-0.5">
+            <p className="text-2xl">{t("conditions")}</p>
+            <ul className="list-inside list-disc ps-1">
+              {booking.unit.terms.map((term, index) => (
+                <li key={index}>{term}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="space-y-0.5">
+            <p className="text-2xl">{t("cancel-policy")}</p>
+            <ul className="list-inside list-disc ps-1">
+              <li>{booking.cancellation_text}</li>
+            </ul>
+          </div>
+
+          <Divider />
+          <div className="space-y-0.5">
+            <p className="text-2xl">{t("support")}</p>
+            <p className="ms-1">{booking.support.label}</p>
+            <ul className="list-inside list-disc ps-1">
+              {booking.support.links.map((link, index) => (
+                <li key={index} className="flex items-center underline">
+                  <a
+                    target="_blank"
+                    href={link.link}
+                    className="flex items-center gap-2"
+                  >
+                    <img className="size-1" src={link.icon_svg} alt="" />
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Stack>
+      </Stack>
     )
   } catch (error) {
     if (axios.isAxiosError(error) && error.response?.status === 404) {
--- src/components/ui/back-button.tsx
+++ src/components/ui/back-button.tsx
@@ -1,10 +1,10 @@
 "use client"
-import { ActionIcon } from "@mantine/core"
+import { ActionIcon, ActionIconProps } from "@mantine/core"
 import { ChevronRight } from "lucide-react"
 import React from "react"
 import { useRouter } from "next/navigation"
 
-type Props = {}
+type Props = ActionIconProps
 
 const BackButton = (props: Props) => {
   const Router = useRouter()
@@ -12,7 +12,13 @@ const BackButton = (props: Props) => {
     Router.back()
   }
   return (
-    <ActionIcon onClick={back} size={"lg"} color="dark" variant="subtle">
+    <ActionIcon
+      onClick={back}
+      size={"lg"}
+      color="dark"
+      variant="subtle"
+      {...props}
+    >
       <ChevronRight size={28} strokeWidth={1.4} className="ltr:rotate-180" />
     </ActionIcon>
   )
