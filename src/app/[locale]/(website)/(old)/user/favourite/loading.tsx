c2b2052e853575439a7b6a679a4c6887bfc2ec1b
Moneer <xv.neer.business@gmail.com>
2025-07-05 09:24:33 +0300

    update users page

--- src/app/[locale]/(website)/(old)/user/favourite/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "@mantine/core"
-
-export default function Loading() {
-  return (
-    <div className="flex h-full w-full items-center justify-center">
-      <Loader />
-    </div>
-  )
-}
