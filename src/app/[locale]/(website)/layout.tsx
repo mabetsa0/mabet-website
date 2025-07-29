eddbbabc56dcfeb3c939ef793415c9c012e1ee3c
Moneer <xv.neer.business@gmail.com>
2025-07-29 12:20:53 +0300

    create go to bookints button

--- src/app/[locale]/(website)/layout.tsx
+++ src/app/[locale]/(website)/layout.tsx
@@ -3,11 +3,11 @@ import { SEO } from "@/services/get-seo"
 import { Metadata } from "next"
 import { hasLocale } from "next-intl"
 import { setRequestLocale } from "next-intl/server"
-import { notFound } from "next/navigation"
-import Navbar from "./components/navbar"
 import dynamic from "next/dynamic"
-import { userAgent } from "next/server"
-import { headers } from "next/headers"
+import { notFound } from "next/navigation"
+import GoToBookings from "./components/go-to-bookings"
+import Navbar from "./components/navbar"
+
 const AuthModal = dynamic(
   () => import("@/components/common/auth/auth-modal"),
   {}
@@ -42,6 +42,7 @@ export default async function LocaleLayout({
       <AuthModal />
       <Nafath />
       <UserDataModal />
+      <GoToBookings />
     </>
   )
 }
