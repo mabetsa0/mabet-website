0cf308c6872f9d9713b3898f12099aabfec4507d
Moneer <xv.neer.business@gmail.com>
2025-11-14 20:42:07 +0200

    Refactor blog layout and page components to adjust dynamic rendering settings; set layout to static and add revalidation for blog pages.

--- src/app/[locale]/blog/layout.tsx
+++ src/app/[locale]/blog/layout.tsx
@@ -2,8 +2,6 @@ import { Cairo } from "next/font/google"
 import Footer from "@/components/blog/footer"
 import { SEO } from "@/services/get-seo"
 
-export const dynamic = "force-dynamic"
-
 const cairo = Cairo({
   subsets: ["arabic"],
   weight: ["700", "600", "500", "400", "300"],
